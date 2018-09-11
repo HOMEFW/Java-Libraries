1b735534eb941294aa283b2a843d6ace8608d252
Quantum <Quantum@192.168.54.36>
2018-09-11 13:40:27 -0300

    b

---
+++ profiles/java/br/com/fws/profiles/data/Base.java
@@ -0,0 +1,5 @@
+package br.com.fws.profiles.data;
+
+public class Base {
+
+}
--- profiles/java/br/com/fws/profiles/entities/UserInformation.java
+++ profiles/java/br/com/fws/profiles/entities/UserInformation.java
@@ -2,4 +2,41 @@ package br.com.fws.profiles.entities;
 
 public class UserInformation extends User {
 
+	private String id;
+	private String name;
+	private String email;
+	private String birthDate;
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getId() {
+		return (super.getUserId().isEmpty() ? id : super.getUserId());
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
